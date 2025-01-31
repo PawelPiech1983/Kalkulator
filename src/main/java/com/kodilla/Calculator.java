e9cf93c2bfe36273254c24f9fe3c35faab0e960a
Paweł Piech <pawel.piech1983@gmail.com>
2025-01-31 13:06:43 +0100

    First commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,15 @@
+package src.main.java.com.kodilla;
+
+public class Calculator {
+    public double a;
+    public double b;
+
+    public double addition(double a, double b) {
+        double result = a + b;
+        return result;
+    }
+    public double subtraction(double a, double b) {
+        double result = a - b;
+        return result;
+    }
+}
